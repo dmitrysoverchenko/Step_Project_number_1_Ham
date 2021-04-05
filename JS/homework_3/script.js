eafe3685e06f8bd04ba8d227f8b6900793fbbc31
Dmytro Soverchenko <isoverchenko@gmail.com>
2021-04-05 19:29:12 +0300

    add hw3 js

---
+++ JS/homework_3/script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+let firstNumber = prompt("Enter first number");
+let secondNumber = prompt("Enter second number");
+let mathOperation = prompt("Enter math operation (+ or - or * or /");
+
+while (
+  isNaN(firstNumber) ||
+  firstNumber === "" ||
+  firstNumber === null ||
+  isNaN(secondNumber) ||
+  secondNumber === "" ||
+  secondNumber === null
+) {
+  firstNumber = prompt("Enter first number", firstNumber);
+  secondNumber = prompt("Enter second number", secondNumber);
+}
+
+const calcOperation = function (firstNumber, secondNumber, mathOperation) {
+  let result;
+  if (mathOperation === "*") {
+    result = +firstNumber * +secondNumber;
+    console.log(result);
+    return result;
+  } else if (mathOperation === "/") {
+    result = +firstNumber / +secondNumber;
+    console.log(result);
+    return result;
+  } else if (mathOperation === "+") {
+    result = +firstNumber + +secondNumber;
+    console.log(result);
+    return result;
+  } else if (mathOperation === "-") {
+    result = +firstNumber - +secondNumber;
+    console.log(result);
+    return result;
+  } else {
+    console.log(`Sorry, "` + mathOperation + `" is not math operation`);
+  }
+};
+
+calcOperation(firstNumber, secondNumber, mathOperation);
