dce4148b36c849deb674a7397fc797f9cc07394f
Dmytro Soverchenko <isoverchenko@gmail.com>
2021-04-06 15:02:00 +0300

    add hw15 js

---
+++ JS/homework_3/script-2.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let number = prompt("Enter your number");
+
+while (isNaN(number) || number === "" || number === null) {
+  number = prompt("Enter your number", number);
+}
+
+const factorial = function (number) {
+  if (number == 1) {
+    return 1;
+  } else {
+    return number * factorial(number - 1);
+  }
+};
+console.log(factorial(number));
