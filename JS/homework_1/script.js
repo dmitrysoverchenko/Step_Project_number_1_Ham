e1302aba3c37ac7cd42b78565fb5f0d05a7a53f8
Dmytro Soverchenko <isoverchenko@gmail.com>
2021-04-07 23:35:16 +0300

    hw1 js cut some code

--- JS/homework_1/script.js
+++ JS/homework_1/script.js
@@ -13,12 +13,12 @@ if (isNaN(+userAge) || userAge == "" || userAge == null || !userName) {
 
 if (userAge <= 17) {
   alert("You are not allowed to visit this website");
-} else if (userAge >= 18 && userAge <= 22) {
+} else if (userAge <= 22) {
   if (confirm("Are you sure you want to continue?")) {
     alert("Welcome, " + userName);
   } else {
     alert("You are not allowed to visit this website");
   }
-} else if (userAge >= 23) {
+} else {
   alert("Welcome, " + userName);
 }
