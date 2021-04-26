c7d3d061387a4f02c874146454ea22852544eaa4
Dmytro Soverchenko <isoverchenko@gmail.com>
2021-04-26 18:52:04 +0300

    45 strings - 39 strings hw 5 js

--- JS/homework_5/script.js
+++ JS/homework_5/script.js
@@ -2,26 +2,20 @@
 
 const user = createNewUser();
 function createNewUser() {
-  let firstName = prompt("enter user's first name");
-  let lastName = prompt("enter user's second name");
-  let birthDate = prompt("enter user's date of birth (dd.mm.yyyy)");
   const newUser = {
-    userFirstName: firstName,
-    userLastName: lastName,
-    userBirthDate: birthDate,
+    userFirstName: prompt("enter user's first name"),
+    userLastName: prompt("enter user's second name"),
+    userBirthDate: prompt("enter user's date of birth (dd.mm.yyyy)"),
     getLogin: function () {
       return this.userFirstName[0] + this.userLastName;
     },
     getAge: function () {
       const now = new Date();
       const currentYear = now.getFullYear();
-      const userBirthYear = +this.userBirthDate.substring(6, 10);
-      const userBirthMonth = +this.userBirthDate.substring(3, 5);
-      const userBirthDay = +this.userBirthDate.substring(0, 2);
       const userDate = new Date(
-        userBirthYear,
-        userBirthMonth - 1,
-        userBirthDay
+        +this.userBirthDate.substring(6, 10),
+        +this.userBirthDate.substring(3, 5) - 1,
+        +this.userBirthDate.substring(0, 2)
       );
       const userYear = userDate.getFullYear();
       let age = currentYear - userYear;
