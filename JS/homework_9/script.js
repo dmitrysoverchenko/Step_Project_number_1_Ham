cbed02344359896a9b7e0adb7538462671bdf265
Dmytro Soverchenko <isoverchenko@gmail.com>
2021-05-20 22:33:07 +0300

    hw9 js done

--- JS/homework_9/script.js
+++ JS/homework_9/script.js
@@ -1,5 +1,12 @@
 "use strict";
 
-const tabsTitle = document.querySelector(".tabs-title");
-console.log(tabsTitle);
-tabsTitle.setAttribute("data-user", "1111");
+const btns = document.querySelectorAll(".tabs-title");
+const items = document.querySelectorAll(".tab-content");
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function (event) {
+    document.querySelector(".tabs-title.active").classList.remove("active");
+    document.querySelector(".tab-content.active").classList.remove("active");
+    event.target.classList.add("active");
+    items[+event.target.getAttribute("data-tab-title")].classList.add("active");
+  });
+}
