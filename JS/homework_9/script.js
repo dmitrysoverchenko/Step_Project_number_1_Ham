e45b3c7a8d08cf26937b1ba77695201966f38e04
Dmytro Soverchenko <isoverchenko@gmail.com>
2021-05-26 18:25:55 +0300

    edit hw9

--- JS/homework_9/script.js
+++ JS/homework_9/script.js
@@ -1,12 +1,18 @@
 "use strict";
 
-const btns = document.querySelectorAll(".tabs-title");
-const items = document.querySelectorAll(".tab-content");
-for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function (event) {
-    document.querySelector(".tabs-title.active").classList.remove("active");
-    document.querySelector(".tab-content.active").classList.remove("active");
+const btns = document.querySelector(".tabs");
+const items = document.querySelector(".tabs-content");
+
+console.log(btns, items, btns.children.length);
+
+btns.addEventListener("click", function (event) {
+  for (let i = 0; i < btns.children.length; i++) {
+    btns.children[i].classList.remove("active");
+    items.children[i].classList.remove("active");
     event.target.classList.add("active");
-    items[+event.target.getAttribute("data-tab-title")].classList.add("active");
-  });
-}
+    items.children[+event.target.getAttribute("data-tab-title")].classList.add(
+      "active"
+    );
+  }
+  console.log(event.target);
+});
