5235c577c0b894c606dbc22e97ddb8659f59d668
Dmytro Soverchenko <isoverchenko@gmail.com>
2021-05-30 18:11:01 +0300

    start hw12 js

--- JS/homework_12/script.js
+++ JS/homework_12/script.js
@@ -1,13 +1,21 @@
 "use strict";
 
-document.addEventListener("keydown", pressKey);
-function pressKey(event) {
-  let btns = document.querySelectorAll(".btn");
-  console.log(event.code, event.key);
-  btns.forEach(function (button) {
-    button.classList.remove("active");
-    if (button.textContent.toUpperCase() === event.key.toUpperCase()) {
-      button.classList.add("active");
+const images = document.querySelectorAll(".image-to-show");
+let position = 1; //0,1,2,3
+
+let timer = setInterval(showImage, 3000);
+
+function showImage() {
+  console.log(position); //ok
+  images.forEach((element, index) => {
+    console.log(element); //ok
+    element.classList.remove("show");
+    element.classList.add("hidden");
+    if (index === position) {
+      element.classList.replace("hidden", "show");
     }
   });
+  if (position < images.length - 1) {
+    position++;
+  } else position = 0;
 }
