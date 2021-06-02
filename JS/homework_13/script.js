93d79131c1bb5ad29a34f3fbf978147d1b13dc74
Dmytro Soverchenko <isoverchenko@gmail.com>
2021-06-03 02:08:31 +0300

    hw13 js finish

--- JS/homework_13/script.js
+++ JS/homework_13/script.js
@@ -4,12 +4,33 @@ console.log(localStorage.getItem("themeColor"));
 
 document.querySelector(".change-button").addEventListener("click", () => {
   if (localStorage.getItem("themeColor") === "normal") {
-    document.querySelector(".menu").classList.add("menu-2");
+    document.querySelector(".change-button").classList.add("menu-orange");
+    document.querySelector(".menu").classList.add("menu-orange");
+    document.querySelector(".footer-area").classList.add("footer-area-orange");
     localStorage.setItem("themeColor", "orange");
     console.log(localStorage.getItem("themeColor"));
   } else {
     localStorage.setItem("themeColor", "normal");
-    document.querySelector(".menu").classList.remove("menu-2");
+    document.querySelector(".change-button").classList.remove("menu-orange");
+    document.querySelector(".menu").classList.remove("menu-orange");
+    document
+      .querySelector(".footer-area")
+      .classList.remove("footer-area-orange");
     console.log(localStorage.getItem("themeColor"));
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("themeColor") === "normal") {
+    document.querySelector(".change-button").classList.remove("menu-orange");
+    document.querySelector(".menu").classList.remove("menu-orange");
+    document
+      .querySelector(".footer-area")
+      .classList.remove("footer-area-orange");
+  } else {
+    document.querySelector(".change-button").classList.add("menu-orange");
+    document.querySelector(".menu").classList.add("menu-orange");
+    document.querySelector(".footer-area").classList.add("footer-area-orange");
+    localStorage.setItem("themeColor", "orange");
   }
 });
