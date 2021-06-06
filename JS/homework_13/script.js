05b5e72ded5093b68f3a27ff2dfdfbfa79aa8270
Dmytro Soverchenko <isoverchenko@gmail.com>
2021-06-06 19:52:51 +0300

    edit funal hw13 js

--- JS/homework_13/script.js
+++ JS/homework_13/script.js
@@ -1,36 +1,35 @@
 "use strict";
 
+function getNormal() {
+  document.querySelector(".change-button").classList.remove("menu-orange");
+  document.querySelector(".menu").classList.remove("menu-orange");
+  document.querySelector(".footer-area").classList.remove("footer-area-orange");
+}
+
+function getOrange() {
+  document.querySelector(".change-button").classList.add("menu-orange");
+  document.querySelector(".menu").classList.add("menu-orange");
+  document.querySelector(".footer-area").classList.add("footer-area-orange");
+  localStorage.setItem("themeColor", "orange");
+}
+
 console.log(localStorage.getItem("themeColor"));
 
 document.querySelector(".change-button").addEventListener("click", () => {
   if (localStorage.getItem("themeColor") === "normal") {
-    document.querySelector(".change-button").classList.add("menu-orange");
-    document.querySelector(".menu").classList.add("menu-orange");
-    document.querySelector(".footer-area").classList.add("footer-area-orange");
-    localStorage.setItem("themeColor", "orange");
+    getOrange();
     console.log(localStorage.getItem("themeColor"));
   } else {
     localStorage.setItem("themeColor", "normal");
-    document.querySelector(".change-button").classList.remove("menu-orange");
-    document.querySelector(".menu").classList.remove("menu-orange");
-    document
-      .querySelector(".footer-area")
-      .classList.remove("footer-area-orange");
+    getNormal();
     console.log(localStorage.getItem("themeColor"));
   }
 });
 
 document.addEventListener("DOMContentLoaded", () => {
   if (localStorage.getItem("themeColor") === "normal") {
-    document.querySelector(".change-button").classList.remove("menu-orange");
-    document.querySelector(".menu").classList.remove("menu-orange");
-    document
-      .querySelector(".footer-area")
-      .classList.remove("footer-area-orange");
+    getNormal();
   } else {
-    document.querySelector(".change-button").classList.add("menu-orange");
-    document.querySelector(".menu").classList.add("menu-orange");
-    document.querySelector(".footer-area").classList.add("footer-area-orange");
-    localStorage.setItem("themeColor", "orange");
+    getOrange();
   }
 });
