bfd7763b9d98a44b5618830ddf5131310bdd17ed
Dmytro Soverchenko <isoverchenko@gmail.com>
2021-06-09 21:16:22 +0300

    add button load more

--- step_project/script.js
+++ step_project/script.js
@@ -24,7 +24,7 @@ ourServicesItem.addEventListener("click", function (event) {
 //OUR WORK FILTER
 
 const ourWork = document.querySelector(".our-amazing-work-menu");
-const ourWorkItems = document.querySelectorAll(".our-amazing-work-grid-item");
+let ourWorkItems = document.querySelectorAll(".our-amazing-work-grid-item");
 console.log(ourWorkItems);
 
 ourWork.addEventListener("click", function (event) {
@@ -48,10 +48,49 @@ ourWork.addEventListener("click", function (event) {
   });
 });
 
+//OUR WORK LOAD ITEMS
+
+function loadImage() {
+  let sum = [];
+  for (let i = 1; i < 13; i++) {
+    if (i < 4) {
+      let workItem = document.querySelector("#wordpress").cloneNode(true);
+      workItem.children[0].src = `./img/our-amazing-work/our-amazing-work-loaded-${i}.jpg`;
+      sum.push(workItem);
+    }
+    if (i >= 4 && i < 7) {
+      let workItem = document.querySelector("#landing-pages").cloneNode(true);
+      workItem.children[0].src = `./img/our-amazing-work/our-amazing-work-loaded-${i}.jpg`;
+      sum.push(workItem);
+    }
+
+    if (i >= 7 && i < 10) {
+      let workItem = document.querySelector("#graphic-desing").cloneNode(true);
+      workItem.children[0].src = `./img/our-amazing-work/our-amazing-work-loaded-${i}.jpg`;
+      sum.push(workItem);
+    }
+
+    if (i >= 10 && i < 13) {
+      let workItem = document.querySelector("#web-design").cloneNode(true);
+      workItem.children[0].src = `./img/our-amazing-work/our-amazing-work-loaded-${i}.jpg`;
+      sum.push(workItem);
+    }
+  }
+  mixImage(sum);
+  return sum;
+}
+
 document.querySelector(".load-more").addEventListener("click", () => {
-  const workItem = document
-    .querySelector(".our-amazing-work-grid-item")
-    .cloneNode(true);
-  console.log(workItem);
-  document.querySelector(".our-amazing-work-grid").append(workItem);
+  document.querySelector(".our-amazing-work-grid").append(...loadImage());
+  document.querySelector(".load-more").remove();
+  ourWorkItems = document.querySelectorAll(".our-amazing-work-grid-item");
 });
+
+function mixImage(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
