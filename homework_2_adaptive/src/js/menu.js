f0f7fc578667a32a1f1d22efc894fc765529285a
Dmytro Soverchenko <isoverchenko@gmail.com>
2021-07-06 02:38:05 +0300

    add header and posts section

--- homework_2_adaptive/src/js/menu.js
+++ homework_2_adaptive/src/js/menu.js
@@ -1,1 +1,5 @@
-console.log("123");
+document.querySelector(".menu--mobile").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.querySelector(".menu--mobile").classList.toggle("active");
+  document.querySelector(".menu").classList.toggle("active");
+});
