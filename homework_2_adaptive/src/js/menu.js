6256772e83d9c24f09a260eb1f1e732e7446f66d
Dmytro Soverchenko <isoverchenko@gmail.com>
2021-07-07 20:01:00 +0300

    finish hw2 adaptive

--- homework_2_adaptive/src/js/menu.js
+++ homework_2_adaptive/src/js/menu.js
@@ -1,5 +1,7 @@
-document.querySelector(".menu--mobile").addEventListener("click", function (e) {
-  e.preventDefault();
-  document.querySelector(".menu--mobile").classList.toggle("active");
-  document.querySelector(".menu").classList.toggle("active");
-});
+document
+  .querySelector(".menu--mobile")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    document.querySelector(".menu--mobile").classList.toggle("active");
+    document.querySelector(".menu").classList.toggle("active");
+  });
