83a5371ca094148f201786dc14ba85802e894286
Dmytro Soverchenko <isoverchenko@gmail.com>
2021-07-28 16:35:29 +0300

    test

--- homework_2_adaptive/src/js/menu.js
+++
@@ -1,7 +0,0 @@
-document
-  .querySelector(".menu--mobile")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    document.querySelector(".menu--mobile").classList.toggle("active");
-    document.querySelector(".menu").classList.toggle("active");
-  });
