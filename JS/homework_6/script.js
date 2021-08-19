277ee9f963f6085f2daac1bef3cea2272d5ecf56
Дмитрий Соверченко <dmitry@MacBook-Air-Dmitrij.local>
2021-08-20 00:49:23 +0300

    edit step for portfolio

--- JS/homework_6/script.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-const arr = ["start", "world", 23, "23", null, true, false, "end", {}];
-const type = "boolean";
-const filterBy = (arr, type) => arr.filter((item) => typeof item !== type);
-console.log(filterBy(arr, type));
--- step_project_2/src/js/menu.js
+++
@@ -1,7 +0,0 @@
-document
-  .querySelector(".menu__mobile")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    document.querySelector(".menu__mobile").classList.toggle("active");
-    document.querySelector(".menu").classList.toggle("active");
-  });
